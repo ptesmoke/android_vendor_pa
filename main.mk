1157a918f6845b6ff13fb261a5b86478e6101234
houst0nn <houstonn@gmail.com>
2014-01-20 01:06:19 -0500

    add extra
    Change-Id: Ie6d1d5270991fa25b05654dbef7bd1b71017e276

--- main.mk
+++ main.mk
@@ -20,5 +20,4 @@ include vendor/pa/configs/system.mk
 include vendor/pa/configs/pa_overrides.mk
 include vendor/pa/configs/bootanimation.mk
 include vendor/pa/configs/version.mk
-
-
+include vendor/pa/configs/extra.mk
